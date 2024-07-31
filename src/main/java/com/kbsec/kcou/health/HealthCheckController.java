afbd9ac54ca331b3ef4ceb6a4e53e7c2c049ab8b
na-yk <ninghao17@naver.com>
2024-07-31 15:07:02 +0900

    feat: health 체크 구현

---
+++ src/main/java/com/kbsec/kcou/health/HealthCheckController.java
@@ -0,0 +1,16 @@
+package com.kbsec.kcou.health;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthCheckController {
+
+    @GetMapping
+    public ResponseEntity<Void> healthCheck() {
+        return ResponseEntity.ok().build();
+    }
+}
